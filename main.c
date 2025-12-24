852b8184feeed0f643f8ad03f21af7f22ed5bdfb
Abhijay <abhijay.stripes@gmail.com>
2025-12-25 03:31:51 +0530

    updated file, no 'result' anymore

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 // writing our own malloc in c - youtube vid 2hr
 #include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
 #include<assert.h>
 
-//fragmentation can happen. thats why malloc is very bad
 
 #define HEAP_CAPACITY 640000
 #define HEAP_ALLOCED_CAP 1024
@@ -13,43 +14,51 @@ typedef struct
     size_t size;
 } Heap_Chunk;
 
-char heap[HEAP_CAPACITY] = {0};  
+char heap[HEAP_CAPACITY] = {1};  
 Heap_Chunk heap_alloced[HEAP_ALLOCED_CAP];
 size_t heap_size = 0;
-
-
+size_t chunks_alloced_size = 0;
 
 
 
 void *heap_alloc(size_t size)
 {
     assert(heap_size + size <= HEAP_CAPACITY && "memory overflow");
+    assert(chunks_alloced_size + size <= HEAP_ALLOCED_CAP && "too many chunks");
+
 
     //void here is like simply saying idc datatype
-    void *ptr1 = heap + heap_size;
-    heap_size += size;
-    return ptr1;
+    void *ptr1 = heap + heap_size; //current free address
+    chunks_alloced_size += size;      
 
     const Heap_Chunk chunk = {
-        .start = result,
+        .start = ptr1,
         .size = size,
     };
+ 
 
-    assert(heap_alloced_size <= HEAP_ALLOCED_CAP);
-    heap_alloced[heap_alloced_size++] = chunk;
 
+    assert(chunks_alloced_size <= HEAP_ALLOCED_CAP);
+
+    heap_alloced[chunks_alloced_size] = chunk;
+    chunks_alloced_size ++;
 
     return ptr1;
+}
+
+
+void heap_dump_alloced_chunks(void)
+{
+    printf("allocated chunks(%zu): \n", chunks_alloced_size);
+    for(size_t i = 0; i < chunks_alloced_size; ++i){
+        printf("start %p, size %zu \n", heap_alloced[i].start, heap_alloced[i].size);
+    }
 }
 
 
 void *heap_free(void *ptr)
 {
     (void) ptr;
-    
-    //assert just double checks that if something youre trying to do
-    //and if actually that much memory required is there, then ok or
-    //else abort 
     assert("TO DO, heap_free not implemented");
 }
 
@@ -63,17 +72,14 @@ void heap_collect()
 
 int main()
 {
-    char *root = heap_alloc(0);
-    for (int i =0; i<26 ; ++i){
-        root[i] = i + 'A';
+    char *root = heap_alloc(4);
+
+    for (int i = 0;i<100; ++i){
+        printf("%d",root[i]); 
     }
+        
+    heap_dump_alloced_chunks();
     heap_free(root);
+    return 0;
 }
 
-
-
-
-//1
-//implement heap allocator,
-//implement heap deallocator
-//implement 
